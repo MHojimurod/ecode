b8896780b8c7d4eff1b0b0c1eda0916fc0ac5444
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2021-11-26 17:14:22 -1200

    final

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from telegram import (
-    KeyboardButton, ChatMember, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton, replykeyboardmarkup, replymarkup, user
+    KeyboardButton, ReplyKeyboardMarkup, InlineQueryResultArticle
 )
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, InlineQueryHandler
 
-from datetime import datetime
 from telegram.ext.callbackcontext import CallbackContext
+from telegram.inline.inputmessagecontent import InputMessageContent
+from telegram.inline.inputtextmessagecontent import InputTextMessageContent
 from telegram.update import Update
 import requests
 
 
 
-my_token = "1955026889:AAFfjBn0PRv-z3-QA_yx2uG_e86XbvF8tho"
+my_token = "2112456432:AAHf4Wz7LR0rZiqZxReJQrhUUsLitTMw-vk"
 
 status = {
     1:"Halol",
@@ -31,18 +32,20 @@ def start_command(update:Update,context:CallbackContext):
 
 def message_handler(update:Update,context:CallbackContext):
     msg = update.message.text
-    res = requests.get(f"http://127.0.0.1:8000/json_ecode/{msg}")
-    # res1 = requests.get(f"http://127.0.0.1:8000/json_name/{msg}")
-    print(res)
+    msg = msg.strip().lower().replace(" ", "")
+    res = requests.get(f"http://157.245.221.46/json_ecode/{msg}")
     a = res.json()['data']
-    if a['code'] ==200 and msg not in ["Halol", "Harom", "Mushbooh"]:
-        text = f"<b>{a['ecode'] if a['ecode'] != '' else 'unknown' }</b>\n<b>{status[a['status']]}</b>\n{a['name']}\n<i>{a['desc']}</i>"   
+    print(a)
+    print(msg)
+
+    if a['code'] == 200 and msg not in ["halol", "harom", "muboh"]:
+        text = f"<b>{a['ecode'] if a['ecode'] != '' else 'unknown' }</b>\n<b>{status[a['status']]}</b>\n{a['name']}\n<i>{a['desc']}</i>"
         update.message.reply_html(text=text) 
-    elif msg not in ["Halol", "Harom", "Mushbooh"] and a['code'] == 404:
+    elif msg not in ["halol", "harom", "muboh"] and a['code'] == 404:
         update.message.reply_html('<b>Siz qidirgan Ecode topilmadi</b>')
 
-    elif msg == "Halol":
-        data = requests.get('http://127.0.0.1:8000/json_status/1/')
+    elif msg == "halol":
+        data = requests.get('http://157.245.221.46/json_status/1/')
         all = data.json()['data']
         print(all)
         text = ''
@@ -53,31 +56,49 @@ def message_handler(update:Update,context:CallbackContext):
         else:
             text = ('<b>afsus hech narsa topilamdi</b>')
         update.message.reply_html(text=text)
-    elif msg == "Harom":
-        data = requests.get('http://127.0.0.1:8000/json_status/2/')
+    elif msg == "harom":
+        data = requests.get('http://157.245.221.46/json_status/2/')
+        all = data.json()['data']
+        text = ''
+        if all != []:
+            for a in all:
+                text += f"<b>{ a['ecode'] if a['ecode'] != '' else 'unknown' }</b>\n<b>{status[a['status']]}</b>\n{a['name']}\n<i>{a['desc']}</i>\n\n" 
+        else:
+            text = ('<b>afsus hech narsa topilamdi</b>')
+        update.message.reply_html(text=text)
+    elif msg == "muboh":
+        data = requests.get('http://157.245.221.46/json_status/3/')
         all = data.json()['data']
         text = ''
         if all != []:
             for a in all:
                 text += f"<b>{a['ecode'] if a['ecode'] != '' else 'unknown' }</b>\n<b>{status[a['status']]}</b>\n{a['name']}\n<i>{a['desc']}</i>\n\n" 
-
         else:
             text = ('<b>afsus hech narsa topilamdi</b>')
         update.message.reply_html(text=text)
-    elif msg == "Mushbooh":
-        data = requests.get('http://127.0.0.1:8000/json_status/3/')
-        all = data.json()['data']
-        text = ''
-        if all != []:
-            for a in all:
-                text += f"<b>{a['ecode'] if a['ecode'] != '' else 'unknown' }</b>\n<b>{status[a['status']]}</b>\n{a['name']}\n<i>{a['desc']}</i>\n\n" 
+    else:
+        start_command(update,context)
 
-        else:
-            text = ('<b>afsus hech narsa topilamdi</b>')
-        update.message.reply_html(text=text)
-
-
+def inline_handler(update:Update, context:CallbackContext):
+    query = update.inline_query.query
+    query = query.strip().lower()
+    res = requests.get(f"http://157.245.221.46/json_data/")
+    results = []
+    for i in res.json()['data']:
+        if query in i['ecode'].lower() or query in i['name'].lower():
+            results.append(InlineQueryResultArticle(
+                id=i['id'],
+                    title=f'{i["ecode"]} | {i["name"]} | {status[i["status"]]}',
+                    input_message_content=InputTextMessageContent(
+                        message_text=f'<b>{i["ecode"]}</b>\n<b>{status[i["status"]]}</b>\n<b><i>{i["name"]}</i></b>\n{i["desc"]}',
+                    parse_mode="HTML"),
+                ))
     
+
+    update.inline_query.answer(
+        results=results,
+        cache_time=10,
+    )
 
     
 
@@ -89,15 +110,12 @@ def main():
     updater = Updater(my_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start_command))
-    # dispatcher.add_handler(CommandHandler("dashboard", admin_command))
     dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
-    # dispatcher.add_handler(CallbackQueryHandler(callback_handler))
-    # dispatcher.add_handler(MessageHandler(Filters.contact, contact_handler))    
+    dispatcher.add_handler(InlineQueryHandler(inline_handler))
 
 
     updater.start_polling()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
